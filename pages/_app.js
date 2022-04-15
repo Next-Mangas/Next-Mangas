7a6c1b2e6c8a5597cfa5e7afc04ac1326694935d
CloudyyUw <cloudyy.contact@gmail.com>
2022-04-15 19:33:07 -0300

    Add semantic ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import 'semantic-ui-css/semantic.min.css';
+import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                ></meta>
+            </Head>
+            <Component {...pageProps} />;
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
